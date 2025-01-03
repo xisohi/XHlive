5b9c582b2b98badbda24295a6aa4a07f0650088f
xihi <xuhui0607@gmail.com>
2025-01-03 12:20:32 +0800

    修复已知BUG

--- app/src/main/java/com/lizongying/mytv0/requests/ReleaseService.kt
+++ app/src/main/java/com/lizongying/mytv0/requests/ReleaseService.kt
@@ -5,7 +5,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface ReleaseService {
-    @GET("main/version.json")
+    @GET("main/update.json")
     fun getRelease(
     ): Call<ReleaseResponse>
 }
